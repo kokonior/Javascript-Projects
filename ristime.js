807019c36169595bbc54afb4c358707b293c4857
RistimeSielnait <91790080+RistimeSielnait@users.noreply.github.com>
2021-10-02 21:16:52 +0700

    Update ristime.js
    Added HTML tags and document.write methode

--- ristime.js
+++ ristime.js
@@ -3,3 +3,23 @@
 document.getElementById("demo").innerHTML = "My Code";
 
 </script>
+
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<h1>My First Web Page</h1>
+
+<p>My first paragraph.</p>
+
+<script>
+
+document.write(5 + 6);
+
+</script>
+
+</body>
+
+</html>
