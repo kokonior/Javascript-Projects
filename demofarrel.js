bd9fdd4981af27c0fb405924cfbe5ed113572cb0
Farrel Wiratama <50479805+Frllzwrtmn@users.noreply.github.com>
2021-10-03 13:46:22 +0700

    Create demofarrel.js

---
+++ demofarrel.js
@@ -0,0 +1,9 @@
+<button onclick="myFunction()">Click Me!</button>
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
+</script>
