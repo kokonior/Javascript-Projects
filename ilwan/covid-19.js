36813cd90fc0c8066f471fe71f8d8f9e0621ccdf
iluser <39887904+iluser@users.noreply.github.com>
2021-10-03 10:31:16 +0800

    Create covid-19.js

---
+++ ilwan/covid-19.js
@@ -0,0 +1,43 @@
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+function covid(){
+	return new Promise(async(resolve, reject) => {
+		axios.get('https://covid19.go.id/')
+		.then(({ data }) => {
+			const $ = cheerio.load(data)
+			const hasil = [];
+			$('#case > div > div > div > div > div:nth-child(2)').each(function(a,b) {
+				const pindo = $(b).find('div:nth-child(3) > strong').text()
+				const mindo = $(b).find('div:nth-child(5) > strong').text()
+				const sindo = $(b).find('div:nth-child(4) > strong').text()
+				const upindo = $(b).find('div.pt-4.text-color-black.text-1').text().trim()
+			$('#case > div > div > div > div > div:nth-child(1)').each(function(c,d) {
+					const neg = $(d).find('div:nth-child(3) > strong').text() 
+					const pglo = $(d).find('div:nth-child(4) > strong').text()
+					const nglo = $(d).find('div:nth-child(5) > strong').text()
+					const up = $(d).find('div.pt-4.text-color-grey.text-1').text().trim()
+				const result = {
+					message: 'ilwan',
+					indo : {
+						positif_indo: pindo,
+						meninggal_indo: mindo,
+						sembuh_indo: sindo,
+						update_indo: upindo.split(':')[1]
+					},
+					global: {
+						negara: neg,
+						positif: pglo,
+						meninggal: nglo,
+						update: up.split(':')[1].split('\n')[0]
+
+					}
+				}
+				hasil.push(result)
+				})
+			})
+			console.log(hasil)
+		})
+		.catch(reject)
+	})
+}
