953df8ecb014b036b958fc23b2ac0a50d684459a
ramadhanu <ramadhanu@locatorlogic.com>
2021-10-03 14:44:01 +0700

    Create triangle printer

---
+++ rzidinc.js
@@ -0,0 +1,49 @@
+const triangles = {
+  normalTriangle: (num) => {
+    let out = "";
+    for (let i = 0; num >= i; i++) {
+      console.log("*".repeat(i));
+    }
+    return out;
+  },
+
+  ascTriangle: (num) => {
+    let out = "";
+    for (let i = 0; num >= i; i++) {
+      if (i > 0) {
+        out += " ".repeat(num - (i - 1)) + "*".repeat(i) + "\n";
+      }
+    }
+    return out;
+  },
+
+  normalReverseTriangle: (num) => {
+    let out = "";
+    for (let i = 0; num >= i; i++) {
+      out += "*".repeat(num - i) + "\n";
+    }
+    return out;
+  },
+
+  reverseAscTriangle: (num) => {
+    let out = "";
+    for (let i = 0; num > i; i++) {
+      out += " ".repeat(i) + "*".repeat(num - i) + "\n";
+    }
+    return out;
+  }
+};
+
+const {
+  normalTriangle,
+  ascTriangle,
+  normalReverseTriangle,
+  reverseAscTriangle
+} = triangles;
+
+const countLoop = 6;
+
+console.log(normalTriangle(countLoop));
+console.log(ascTriangle(countLoop));
+console.log(normalReverseTriangle(countLoop));
+console.log(reverseAscTriangle(countLoop));
