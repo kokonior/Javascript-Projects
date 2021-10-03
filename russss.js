9b15aae2d703c633722d846cedb544c2f6bb243e
Muhammad Rusadi Rahmat <91780576+rusadirr@users.noreply.github.com>
2021-10-03 15:30:46 +0700

    Create russss.js

---
+++ russss.js
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int time = 22;
+    if (time < 10) {
+      System.out.println("Good morning.");
+    } else if (time < 20) {
+      System.out.println("Good day.");
+    }  else {
+      System.out.println("Good evening.");
+    }
+  }
+}
