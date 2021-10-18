7f197356075600d9a3ff6f6257c5992b96ba5a61
Fio Prasetyo <48793360+fiopras@users.noreply.github.com>
2021-10-18 11:30:03 +0700

    Create fiopras.js

---
+++ fiopras.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Objects CARS</h2>
+
+<p id="mobil"></p>
+
+<script>
+// Create an object:
+const car = {type:"Avanza", model:"2000", color:"red"};
+
+// Display some data from the object:
+document.getElementById("mobil").innerHTML = "The car type is " + car.type;
+</script>
+
+</body>
+</html>
