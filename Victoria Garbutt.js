9f1f323acbca5a4d1914f57e4e69f67a0ecb4d85
VictoriaGarbutt12 <93401614+VictoriaGarbutt12@users.noreply.github.com>
2021-10-30 00:58:15 +0700

    Update Victoria Garbutt.js

--- Victoria Garbutt.js
+++ Victoria Garbutt.js
@@ -3,3 +3,9 @@ function pl() {
     let ot = document.getElementById("ot");
 
     let vowels = ["a", "e", "i", "o", "u"];
+
+    let newStr = "";
+    let stra = str.split(" ");
+
+    for (s in stra) {
+        let ss = stra[s];
