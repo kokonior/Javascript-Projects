08be748933d7b155291216ea5522e442e9e4ec6e
Raihan Nurrohman <78289708+raihannr30@users.noreply.github.com>
2021-10-29 23:56:29 +0700

    Create Raihan.js

---
+++ Raihan.js
@@ -0,0 +1,6 @@
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
