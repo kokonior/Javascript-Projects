5bd0cf704295ac9e521addc01f3e3e4f13731eed
GaeColton12 <93404909+GaeColton12@users.noreply.github.com>
2021-10-30 03:08:07 +0700

    Update GaeColton.js

--- GaeColton.js
+++ GaeColton.js
@@ -26,6 +26,27 @@ function updateRange() {
   highValue.style.background = "#ef7b54";
 }
 
+function updateRange() {
+  const rangeOutput = document.getElementById("rangeOutput");
+  rangeOutput.innerText = `${low} - ${high}`;
+  rangeOutput.style.marginLeft = low + "%";
+  rangeOutput.style.marginRight = 100 - high + "%";
+  rangeOutput.classList.add("flash");
+
+  const lowValue = document.getElementById("low");
+  lowValue.style.flex = low + "%";
+  lowValue.style.background = "#ef7b54";
+
+  const space = document.getElementById("space");
+  space.style.flex = high - low + "%";
+  space.style.background = "#83E1D0";
+
+  const highValue = document.getElementById("high");
+  if (high == 100) highValue.style.flex = 0;
+  highValue.style.flex = 100 - high + "%";
+  highValue.style.background = "#ef7b54";
+}
+
 function gameEnded() {
   document.getElementById("newGameButton").style.display = "inline";
   document.getElementById("inputBox").setAttribute("readonly", "readonly"); // (attr name, attr value)
