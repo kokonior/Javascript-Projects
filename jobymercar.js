282b5b98245a4ff456471c8ac1d44f67576c5dfa
jobymercar <93408268+jobymercar@users.noreply.github.com>
2021-10-30 04:15:08 +0700

    Update jobymercar.js

--- jobymercar.js
+++ jobymercar.js
@@ -3,3 +3,10 @@ new Vue({
   data:{
       image:''
   },
+
+  methods:{
+    onFileChange(e){
+      var files=e.target.files || e.dataTransfer.files;
+      if(!files.length)
+          return;
+      this.createImage(files[0]);
