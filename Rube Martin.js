3eb2a8606bd2a0d016d549efebcb177664b4b305
RubeMartin12 <93406868+RubeMartin12@users.noreply.github.com>
2021-10-30 03:26:53 +0700

    Update Rube Martin.js

--- Rube Martin.js
+++ Rube Martin.js
@@ -5,3 +5,15 @@ let board = [
     [0, 0, 1, 0]
 ];
 
+//first loop for 4 nested array
+function change1tox(board) {
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            if (board[i][j] === 1) {
+                board[i][j] = 'x'
+            }
+        }
+
+    }
+    return board
+}
