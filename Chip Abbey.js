7a6db501889c763b1c2398df091b06d055145688
ChipAbbey12 <93402275+ChipAbbey12@users.noreply.github.com>
2021-10-30 01:14:43 +0700

    Create Chip Abbey.js

---
+++ Chip Abbey.js
@@ -0,0 +1,23 @@
+let flag=0;
+let position=1;
+let exp;
+const display = document.querySelector(".display");
+const keys=document.querySelectorAll('input');
+const scroller=document.querySelector(".circle");
+
+function reset(){
+    display.textContent='0';
+    exp='0';
+    flag=0;
+}
+
+function del(){
+    if(exp.length==1){
+        flag=0;
+        display.textContent='0';
+        return;
+    }
+  exp=exp.slice(0,-1);
+  display.textContent=exp;
+  
+}
