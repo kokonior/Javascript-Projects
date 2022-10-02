9680746411dd7f6ffb886552ad413ba1f57bbaee
Risky Muhamad <42082391+Riskymuhamad@users.noreply.github.com>
2022-10-02 13:57:26 +0700

    Create array_chalenges.js
    hactoberfest2022

---
+++ array_chalenges.js
@@ -0,0 +1,11 @@
+var makanan = ['jengkol','garam','santan','penyedap']
+
+makanan.splice(2, 0, 'kecap')
+console.log(makanan)
+
+makanan.splice(4, 0, 'minyak goreng')
+console.log(makanan)
+
+
+makanan.splice(1,1,'mecin')
+console.log(makanan)
