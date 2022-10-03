bbfd4208882e398535972ab66722e13dd7ff12ac
Fujin Ore <84015052+fujinore@users.noreply.github.com>
2022-10-03 09:24:08 +0700

    Update bagusxbang6.js

--- bagusxbang6.js
+++ bagusxbang6.js
@@ -1,32 +1,53 @@
-setPreFight: function(){
-        let getHeader = document.querySelector(".header");
-        let getActions = document.querySelector(".actions");
-        let getArena = document.querySelector(".arena");
-        getHeader.innerHTML = '<p>Task: Find an Enemy!</p>';
-        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for an enemy!</a>';
-        getArena.style.visibility = "visible";
-    },
-
-    setFight: function(){
-        let getHeader = document.querySelector(".header");
-        let getActions = document.querySelector(".actions");
-        let getEnemy = document.querySelector(".enemy");
-        
-        //Create enemy!
-
-        let enemy00 = new Enemy("Goblin", 100, 0, 50, 100, 100);
-        let enemy01 = new Enemy("Troll", 200, 0, 150, 80, 150);
-        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
-        switch (chooseRandomEnemy){
-            case 0:
-                enemy = enemy00;
-                break;
-            case 1:
-                enemy = enemy01;
-                break;
+//initiate attacks!
+    if(getPlayerSpeed >= getEnemySpeed){
+        let playerAttackValues = playerAttack();
+        let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+        enemy.health = enemy.health - totalDamage;
+        alert("You hit" + playerAttackValues[0] + "damage" + playerAttackValues[1] + "times.");
+        if(enemy.health<=0){
+            alert("You win! Refresh browser to play again.");
+            getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            getEnemyHealth.innerHTML = 'Health: 0';
+        }else{
+            getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            //enemy attacks
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+            player.health = player.health - totalDamage;
+            alert("Enemy hit" + enemyAttackValues[0] + "damage" + enemyAttackValues[1] + "times.");
+            if(player.health<=0){
+            alert("You loose! Refresh browser to play again.");
+            getPlayerHealth.innerHTML = 'Health: 0';
+            getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+            }else{
+                getPlayerHealth.innerHTML = 'Health: ' + player.health;
+            }
         }
-        getHeader.innerHTML = '<p>Task: Choose your move!</p>';
-        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-        getEnemy.innerHTML = '<img src="img/'+ enemy.enemyType.toLowerCase() +'.jpg" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    }
+    else if(getEnemySpeed >= getPlayerSpeed){
+        let enemyAttackValues = enemyAttack();
+        let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+        player.health = player.health - totalDamage;
+        alert("Enemy hit" + enemyAttackValues[0] + "damage" + enemyAttackValues[1] + "times.");
+        if(player.health<=0){
+            alert("You loose! Refresh browser to play again.");
+            getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            getPlayerHealth.innerHTML = 'Health: 0';
+        }else{
+            getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            //Player attacks
+            let playerAttackValues = playerAttack();
+            let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+            enemy.health = enemy.health - totalDamage;
+            alert("You hit" + playerAttackValues[0] + "damage" + playerAttackValues[1] + "times.");
+            if(enemy.health<=0){
+            alert("You win! Refresh browser to play again.");
+            getEnemyHealth.innerHTML = 'Health: 0';
+            getPlayerHealth.innerHTML = 'Health: ' + player.health;
+            }else{
+                getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+            }
+        }
+    }
     }
 }
