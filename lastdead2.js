30f5ead2b712600623d6c1e8701770c591d543c7
Joe Othenon <83144695+joeothenons@users.noreply.github.com>
2022-10-03 08:44:11 +0700

    Update lastdead2.js

--- lastdead2.js
+++ lastdead2.js
@@ -23,3 +23,36 @@ let GameManager ={
         let getInterface = document.querySelector(".interface");
         getInterface.innerHTML = '<img src="img/'+ classType.toLowerCase() +'.jpg" class="img-avatar"><div><h3>' + classType + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
     },
+
+    setPreFight: function(){
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getArena = document.querySelector(".arena");
+        getHeader.innerHTML = '<p>Task: Find an Enemy!</p>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for an enemy!</a>';
+        getArena.style.visibility = "visible";
+    },
+
+    setFight: function(){
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
+        
+        //Create enemy!
+
+        let enemy00 = new Enemy("Goblin", 100, 0, 50, 100, 100);
+        let enemy01 = new Enemy("Troll", 200, 0, 150, 80, 150);
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+        switch (chooseRandomEnemy){
+            case 0:
+                enemy = enemy00;
+                break;
+            case 1:
+                enemy = enemy01;
+                break;
+        }
+        getHeader.innerHTML = '<p>Task: Choose your move!</p>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+        getEnemy.innerHTML = '<img src="img/'+ enemy.enemyType.toLowerCase() +'.jpg" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    }
+}
