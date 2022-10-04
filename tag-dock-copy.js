d2c7d92fd2136187bc255931825687b0a44d2b96
JnckCode <54052722+jnckcode@users.noreply.github.com>
2022-10-04 17:33:13 +0700

    Create tag-dock-copy.js
    added some features for copying result to clipboard automaticly

---
+++ tag-dock-copy.js
@@ -0,0 +1,15 @@
+/* Do not change this script DWYOR
+===== Created by jnckcode ========
+*/
+
+      $("#copas").click(function(){
+         $("#result").select();
+         document.execCommand('copy');
+      });
+
+      $(document).getElementById('#Gass'){
+      	var form = document.getElementById('#form');
+      	var button = document.getElementById('#button-send');
+      	form.select();
+      	button.copy();
+      }
