7dffffd05b400278e21a62c94330372b32a82b12
ahmadscript <115629297+ahmadscript@users.noreply.github.com>
2022-10-13 21:42:26 +0700

    Update ahmadfauzan.js

--- ahmadfauzan.js
+++ ahmadfauzan.js
@@ -1,25 +1,21 @@
-window.onload = () => {
-   // Creating helper function to set multiple attributes
-   function setAttribute(elmName,attribute){
-      for(let key in attribute){
-         elmName.setAttribute('elmName','attribute[key]');
-      }
-   }
+const capital = new Map([
+    ["Jakarta", "Indonesia"],
+    ["London", "England"],
+    ["Tokyo", "Japan"],
+    ["Lisbon","Portugal"]
+]);
 
-   var html = document.getElementsByTagName('html');
-   html.style.innerText = `font-size: 12px;font-weight: 300;`;
+console.log(capital.size);
+console.log(capital.get("London"));
+capital.set("New Delhi", "India");
+console.log(capital.size);
+console.log(capital.get("New Delhi"));
+console.log(capital.get("Lisbon"));
+console.log(capital.size);
 
-   var body = document.getElementsByTagName('body');
-   body.style.width = '90vw';
-
-   var main = document.createElement('main');
-   var startScreen = document.createElement('div');   // create a div
-   startScreen.setAttribute('id','startScreen');   // setting id to div
-   var note1 = document.createElement('h1');
-   note1.innerText = "Guess the Random number between 1-100";
-   startScreen.appendChild(note1);    // added a heading  to start screen
-
-   main.appendChild(startScreen);            // added a div to main
-   document.body.appendChild(main);          // added a main in body
-
-}
+/* output
+3
+England
+4
+India
+ */
