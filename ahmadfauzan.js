3a649926099f5e2facfd15526c66221b32331963
ahmadscript <115629297+ahmadscript@users.noreply.github.com>
2022-10-13 21:41:54 +0700

    Update ahmadfauzan.js

--- ahmadfauzan.js
+++ ahmadfauzan.js
@@ -1,34 +1,25 @@
-new Vue({
-  el:'#app',
-  data:{
-      image:''
-  },
+window.onload = () => {
+   // Creating helper function to set multiple attributes
+   function setAttribute(elmName,attribute){
+      for(let key in attribute){
+         elmName.setAttribute('elmName','attribute[key]');
+      }
+   }
 
-  methods:{
-    onFileChange(e){
-      var files=e.target.files || e.dataTransfer.files;
-      if(!files.length)
-          return;
-      this.createImage(files[0]);
+   var html = document.getElementsByTagName('html');
+   html.style.innerText = `font-size: 12px;font-weight: 300;`;
 
-      },
-    createImage(file){
-        var image = new Image();
-        var reader = new FileReader();
-        var vm=this;
-        reader.onload=(e)=>{
-              vm.image=e.target.result;
-        };
-        reader.readAsDataURL(file);
-    },
+   var body = document.getElementsByTagName('body');
+   body.style.width = '90vw';
 
-    removeImage:function(e){
-      this.image='';
-    }
-  }
-})
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
+   var main = document.createElement('main');
+   var startScreen = document.createElement('div');   // create a div
+   startScreen.setAttribute('id','startScreen');   // setting id to div
+   var note1 = document.createElement('h1');
+   note1.innerText = "Guess the Random number between 1-100";
+   startScreen.appendChild(note1);    // added a heading  to start screen
+
+   main.appendChild(startScreen);            // added a div to main
+   document.body.appendChild(main);          // added a main in body
+
+}
