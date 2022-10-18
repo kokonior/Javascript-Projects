458d5548409d824953551230c3eefd501cb30c47
MyName49 <33026946+MyName49@users.noreply.github.com>
2022-10-18 21:33:01 +0700

    Create accbanh.js

---
+++ accbanh.js
@@ -0,0 +1,7 @@
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
