1d33f9d32f98de838aea1c102eb2b18359da5bca
hnifmaghfur <hnifmaghfur@gmail.com>
2023-10-23 11:18:22 +0700

    add password generator

---
+++ password-generator/index.js
@@ -0,0 +1,11 @@
+function generatePassword(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+console.log(generatePassword(10)); // Example usage: generates a 10-character password
